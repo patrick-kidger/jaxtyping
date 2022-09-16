b36f6b47a6f71b946602ec74a7487199b5b2adec
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-09-16 07:58:29 -0700

    Update test_threading.py

--- test/test_threading.py
+++ test/test_threading.py
@@ -20,19 +20,20 @@
 import threading
 
 import jax.numpy as jnp
-from jaxtyping import Array, Float, jaxtyped
 from typeguard import typechecked
+
+from jaxtyping import Array, Float, jaxtyped
 
 
 def test_threading():
     @jaxtyped
     @typecheckef
-    def add(x: Float[Array, 'a b'], y: Float[Array, 'a b']) -> F[Array, 'a b']:
+    def add(x: Float[Array, "a b"], y: Float[Array, "a b"]) -> Float[Array, "a b"]:
         return x + y
 
     def run():
-        a = jnp.array([[1., 2.]])
-        b = jnp.array([[2., 3.]])
+        a = jnp.array([[1.0, 2.0]])
+        b = jnp.array([[2.0, 3.0]])
         add(a, b)
 
     thread = threading.Thread(target=run)
