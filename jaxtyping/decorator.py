d911ebb99cf6e2c3983d633625dbd1a7abc409af
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-01-22 11:43:36 -0800

    Fix abstractmethods being ignored after @jaxtyped

--- jaxtyping/decorator.py
+++ jaxtyping/decorator.py
@@ -31,7 +31,12 @@ class _Jaxtyped:
         self.fn = fn
 
     def __get__(self, instance, owner):
-        return ft.wraps(self.fn)(_Jaxtyped(self.fn.__get__(instance, owner)))
+        if instance is None:
+            # Don't create a new _Jaxtyped object in this case. Otherwise anything
+            # assigned to methods (e.g. `__isabstractmethod__`) just gets swallowed.
+            return self
+        else:
+            return ft.wraps(self.fn)(_Jaxtyped(self.fn.__get__(instance, owner)))
 
     def __call__(self, *args, **kwargs):
         try:
