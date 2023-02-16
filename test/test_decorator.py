81c56052e50282a3e28b3505b6640da6654cdd9e
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-02-16 21:08:57 +0300

    Fixes for some new failures. (Where did they come from?) (#65)
    * Fixes for some new failures. (Where did they come from?)
    
    * Fixed isort?

--- test/test_decorator.py
+++ test/test_decorator.py
@@ -1,13 +1,26 @@
+import abc
+
 from jaxtyping import jaxtyped
 
 
-class M:
+class M(metaclass=abc.ABCMeta):
     @jaxtyped
     @classmethod
     def f(cls):
         return 3
 
+    @jaxtyped
+    @abc.abstractmethod
+    def g(self):
+        ...
+
 
 # Check that the @jaxtyped decorator doesn't blat the __get__ of @classmethod
-def test_decorator():
+def test_classmethod():
     assert M.f() == 3
+
+
+# Check that the @jaxtyped decorator doesn't blat the __isabstractmethod__ of
+# @abstractmethod
+def test_abstractmethod():
+    assert M.g.__isabstractmethod__
