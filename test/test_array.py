9beb5f2d2955bd3d94c04057bf6d3a3585e6aec6
jianlijianli <43797323+jianlijianli@users.noreply.github.com>
2024-02-16 17:34:47 -0800

    Add int4/uint4 support in jaxtyping. (#174)
    * Add int4/uint4 support in jaxtyping.
    
    * Fix a typo and update api docs.

--- test/test_array.py
+++ test/test_array.py
@@ -67,6 +67,7 @@ def test_dtypes():
         Float64,
         Inexact,
         Int,
+        Int4,
         Int8,
         Int16,
         Int32,
@@ -74,6 +75,7 @@ def test_dtypes():
         Num,
         Shaped,
         UInt,
+        UInt4,
         UInt8,
         UInt16,
         UInt32,
@@ -125,7 +127,9 @@ def test_any_dtype(jaxtyp, typecheck, getkey):
 
     g(jr.normal(getkey(), (3, 4)))
     g(jnp.array([[True, False]]))
+    g(jnp.array([[1, 2], [3, 4]], dtype=jnp.int4))
     g(jnp.array([[1, 2], [3, 4]], dtype=jnp.int8))
+    g(jnp.array([[1, 2], [3, 4]], dtype=jnp.uint4))
     g(jnp.array([[1, 2], [3, 4]], dtype=jnp.uint16))
     g(jr.normal(getkey(), (3, 4), dtype=jnp.complex128))
     g(jr.normal(getkey(), (3, 4), dtype=jnp.bfloat16))
