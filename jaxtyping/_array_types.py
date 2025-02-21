c7b652986355049b3910b3cc0d211e169f76ffc8
Gabriel de Marmiesse <gabrieldemarmiesse@Kyutais-Laptop.local>
2025-02-21 11:05:02 +0100

    Formatting

--- jaxtyping/_array_types.py
+++ jaxtyping/_array_types.py
@@ -620,9 +620,7 @@ def _make_array(x, dim_str, dtype):
         # Nanobind classes can't be subclassed
         can_subclass = array_type is Any or "nanobind." in str(type(array_type))
         metaclass = (
-            _make_metaclass(type)
-            if can_subclass
-            else _make_metaclass(type(array_type))
+            _make_metaclass(type) if can_subclass else _make_metaclass(type(array_type))
         )
         out = metaclass(
             name,
