f0f9aded81937f89ad3fc0b1850d851a7813d79c
Gabriel de Marmiesse <gabrieldemarmiesse@Kyutais-Laptop.local>
2025-02-21 10:56:21 +0100

    Allow MLX types to be recognized

--- jaxtyping/_array_types.py
+++ jaxtyping/_array_types.py
@@ -617,15 +617,16 @@ def _make_array(x, dim_str, dtype):
 
     if type(out) is tuple:
         array_type, name, dtypes, dims, index_variadic, dim_str = out
+        # Nanobind classes can't be subclassed
+        can_subclass = array_type is Any or "nanobind." in str(type(array_type))
         metaclass = (
             _make_metaclass(type)
-            if array_type is Any
+            if can_subclass
             else _make_metaclass(type(array_type))
         )
-
         out = metaclass(
             name,
-            (AbstractArray,) if array_type is Any else (array_type, AbstractArray),
+            (AbstractArray,) if can_subclass else (array_type, AbstractArray),
             dict(
                 dtype=dtype,
                 array_type=array_type,
